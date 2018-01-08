046d0b30b425cce11c39ed18d1b59db51aa4d5c9
Crepuscule <yuji199509@gmail.com>
2018-01-08 09:45:07 -0800

    changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();   // can have different apps
 
 // route-handler
 app.get('/',(req,res) => {
-    res.send({hi:'there'});
+    res.send({bye:'there'});
 });
 
 const PORT = process.env.PORT || 5000;
